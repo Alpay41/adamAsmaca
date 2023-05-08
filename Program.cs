658f0edf02637b7de5ac8d27ff428bb5b6f38084
Alpay Başol <alpay41@gmail.com>
2023-05-08 18:00:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace adamAsmaca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adam Asmaca Oyunu");
+
+        }
+    }
+}
